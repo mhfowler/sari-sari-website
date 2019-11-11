5f78b0df37088964693cf7068860a4f11e9a1ec4
Max Fowler <maxhfowler@gmail.com>
2019-11-11 12:25:49 +0100

    Working on mobile responsive

--- wordpress/wp-content/themes/sariwebsitesimple/index.php
+++ wordpress/wp-content/themes/sariwebsitesimple/index.php
@@ -5,6 +5,7 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
     <script src="<?php echo get_bloginfo('template_directory'); ?>/moment.js"></script>
     <script src="<?php echo get_bloginfo('template_directory'); ?>/app.js"></script>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<title>sari sari</title>
 </head>
 <body>
